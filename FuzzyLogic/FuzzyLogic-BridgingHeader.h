d2c558087032fd1547308e1182ab8a34d03939f1
Victor Shamanov <wiruzx@gmail.com>
2015-03-18 00:54:32 +0300

    Add bridging header

---
+++ FuzzyLogic/FuzzyLogic-BridgingHeader.h
@@ -0,0 +1,11 @@
+//
+//  FuzzyLogic-BridgingHeader.h
+//  FuzzyLogic
+//
+//  Created by Victor Shamanov on 3/18/15.
+//  Copyright (c) 2015 Victor Shamanov. All rights reserved.
+//
+
+#import "FSLineChart.h"
+#import "UIColor+FSPalette.h"
+#import "YapDatabase.h"
