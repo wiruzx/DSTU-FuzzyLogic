776a345179a62050388c3dbd1bb4fc0ae99d1e27
Victor Shamanov <wiruzx@gmail.com>
2015-03-19 23:58:53 +0300

    Add corner cases for functions

--- FuzzyLogic/QueryFunctions.swift
+++ FuzzyLogic/QueryFunctions.swift
@@ -9,31 +9,45 @@
 import Foundation
 
 func five(value: Double) -> Double {
-    return (80 - value) / 10
+    switch value {
+    case let x where x >= 0 && x <= 100:
+        return (80 - x) / 10
+    default:
+        return 0
+    }
 }
 
 func four(value: Double) -> Double {
     switch value {
-    case let x where x < 70:
+    case let x where x < 70 && x >= 0:
         return (60 - x) / 10
     case let x where x >= 70 && x <= 80:
         return 1
+    case let x where x > 80 && x <= 100:
+        return (90 - x) / 10
     default:
-        return (90 - value) / 10
+        return 0
     }
 }
 
 func three(value: Double) -> Double {
     switch value {
-    case let x where x < 40:
+    case let x where x < 40 && x >= 0:
         return (x - 30) / 10
     case let x where x >= 40 && x <= 60:
         return 1
+    case let x where x > 60 && x <= 100:
+        return (70 - x) / 10
     default:
-        return (70 - value) / 10
+        return 0
     }
 }
 
 func two(value: Double) -> Double {
-    return (40 - value) / 10
+    switch value {
+    case let x where x >= 0 && x <= 100:
+        return (40 - x) / 10
+    default:
+        return 0
+    }
 }
